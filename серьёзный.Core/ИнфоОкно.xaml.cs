56c2664ca6044ec2143a53d4b390bab5c100f953
eridan-forge <eridanforge@gmail.com>
2026-09-14 20:33:05 +0300

    rye

---
+++ серьёзный.Core/ИнфоОкно.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace серьёзный.ЭкранКлуба;
+
+public partial class ИнфоОкно : Window
+{
+    public ИнфоОкно(string сообщение, string заголовок = "Уведомление")
+    {
+        InitializeComponent();
+
+        ТекстЗаголовок.Text = заголовок;
+        ТекстСообщения.Text = сообщение;
+    }
+
+    private void Закрыть_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+}
